957181155f93de071144bf097f5ceb4c60bedcf6
munguriek <benjaminmunguriek@gmail.com>
2020-06-17 01:10:27 +0300

    Initial Commit of the django blog app with designed data models, views, templates, URLs and object pagination

---
+++ blog_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\munguriek\projects\django3byexample\blogapp\blog_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
